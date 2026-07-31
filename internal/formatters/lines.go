4406f4b700f93c08628985daef7761fa161f7d02
coderiaser <coderaiser@cloudcmd.io>
2026-08-01 00:53:16 +0300

    feature: formatters: line: add file:// only in webstorm terminal

--- internal/formatters/lines.go
+++ internal/formatters/lines.go
@@ -2,6 +2,7 @@ package formatters
 
 import (
 	"fmt"
+	"os"
 
 	"coderaiser/go-coverage/internal/block"
 )
@@ -9,9 +10,15 @@ import (
 type Lines struct{}
 
 func (Lines) Format(b block.Block) string {
-	if b.Start == b.End {
-		return fmt.Sprintf("file://%s:%d: %d", b.File, b.Start, b.Start)
+	prefix := ""
+
+	if os.Getenv("TERMINAL_EMULATOR") == "JetBrains-JediTerm" {
+		prefix = "file://"
 	}
 
-	return fmt.Sprintf("file://%s:%d: %d-%d", b.File, b.Start, b.Start, b.End)
+	if b.Start == b.End {
+		return fmt.Sprintf("%s%s:%d: %d", prefix, b.File, b.Start, b.Start)
+	}
+
+	return fmt.Sprintf("%s%s:%d: %d-%d", prefix, b.File, b.Start, b.Start, b.End)
 }
--- internal/formatters/lines_test.go
+++ internal/formatters/lines_test.go
@@ -1,6 +1,7 @@
 package formatters_test
 
 import (
+	"os"
 	"testing"
 
 	"coderaiser/go-coverage/internal/block"
@@ -11,14 +12,36 @@ import (
 
 func TestLines(t *testing.T) {
 	Test(t, "lines: range", func(t *T) {
-		result := formatters.Lines{}.Format(block.Block{File: "main.go", Start: 10, End: 12})
-		t.Equal(result, "file://main.go:10: 10-12")
+		t.Setenv("TERMINAL_EMULATOR", "")
+		result := formatters.Lines{}.Format(block.Block{
+			File:  "main.go",
+			Start: 10,
+			End:   12,
+		})
+		t.Equal(result, "main.go:10: 10-12")
 		t.End()
 	})
 
 	Test(t, "lines: single line omits range", func(t *T) {
-		result := formatters.Lines{}.Format(block.Block{File: "main.go", Start: 24, End: 24})
-		t.Equal(result, "file://main.go:24: 24")
+		t.Setenv("TERMINAL_EMULATOR", "")
+		result := formatters.Lines{}.Format(block.Block{
+			File:  "main.go",
+			Start: 24,
+			End:   24,
+		})
+		t.Equal(result, "main.go:24: 24")
 		t.End()
 	})
+
+	Test(t, "lines: prepends file:// in WebStorm terminal", func(t *T) {
+		t.Setenv("TERMINAL_EMULATOR", "JetBrains-JediTerm")
+		result := formatters.Lines{}.Format(block.Block{
+			File:  "main.go",
+			Start: 10,
+			End:   12,
+		})
+		t.Equal(result, "file://main.go:10: 10-12")
+		t.End()
+	})
+}
 }
